5f6fdff405c0a0dc018cc544ff581320b904518f
Mohamed Ayari <ayari.mohamed@web.de>
2025-05-07 02:20:43 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+          alt="headerImage"
+          className="app__headerImage"
+        />
+      </div>
+      <h1>Welcome to React </h1>
+
+    </div>
+  );
+}
+
+export default App;
