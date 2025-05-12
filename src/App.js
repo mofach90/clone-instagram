babe973673ee500e03c62a1e5a53fe9dc1c0796f
Mohamed Ayari <ayari.mohamed@web.de>
2025-05-12 12:52:30 +0200

    Add lisner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { db } from "./firebase.js";
@@ -8,25 +8,29 @@ function App() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const querySnapshot = await getDocs(collection(db, "first-collection"));
-        console.log("Data fetched from Firestore", querySnapshot);
-        querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
-          console.log("Data fetched from Firestore", doc.data());
-          const post = {
-            id: doc.id,
-            caption: doc.data().caption,
-            userName: doc.data().user_name,
-            postImage: doc.data().image_url,
-            avatarImage: doc.data().avatar_image,
-          };
-          setPosts((prevPosts) => [...prevPosts, post]);
-        });
-      } catch (e) {
-        console.error(e);
-      }
+    function fetchData() {
+      
+      const querySnapshot = onSnapshot(
+        collection(db, "first-collection"),
+        (collection) => {
+          setPosts((posts) => []);
+          collection.forEach((doc) => {
+            console.log("Data fetched from Firestore", typeof doc.data());
+            const post = {
+              id: doc.id,
+              caption: doc.data().caption,
+              userName: doc.data().user_name,
+              postImage: doc.data().image_url,
+              avatarImage: doc.data().avatar_image,
+            };
+            
+            setPosts((posts) => [...posts, post]);
+            console.log("Post data", post);
+            console.log("Posts data", posts);
+          });
+        }
+      );
+      // const querySnapshot = await getDocs(collection(db, "first-collection"));
     }
     fetchData();
     console.log("Data fetched from Firestore", posts);
